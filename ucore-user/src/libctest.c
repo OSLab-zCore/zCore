6eb7a31b0d6286e04adaaa742f7d029933a5f7b3
Zichun Yu <yuzc19@gmail.com>
2022-05-28 23:36:06 +0800

    Update libctest.c

--- ucore-user/src/libctest.c
+++ ucore-user/src/libctest.c
@@ -13,7 +13,37 @@ char buf[INSIZE];
 
 int main() {
   int FD = open("riscv64_bare.txt", O_RDWR | O_CREAT);
-  read(FD, buf, INSIZE);
-  for (int i = 0; i < 10; i++) cprintf("");
+  int len = read(FD, buf, INSIZE);
+  int i = 0, begin = 0, end = 0;
+  while (i < len) {
+    while (buf[i] != ' ') {
+      i++;
+      end++;
+    }
+    while (buf[i] == ' ') {
+      i++;
+    }
+    while (buf[i] != '\n') {
+      i++;
+    }
+    if (buf[i - 1] == 'K') {
+      char name[end - begin + 1];
+      memcpy(name, buf + begin, end - begin);
+      name[end - begin] = '\0';
+      if (fork() == 0) {
+        int returncode = sys_exec(name, 0, NULL);
+        if (returncode) {
+          cprintf("%s FAILED\n", name);
+        } else {
+          cprintf("%s OK\n", name);
+        }
+        exit(0);
+      }
+    }
+    i++;
+    begin = i, end = i;
+    // while (wait() <= 0) {
+    // }
+  }
   return 0;
 }
